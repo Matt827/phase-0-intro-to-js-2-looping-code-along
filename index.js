8b090d9151d85e5e55cc5514d2077286ac5b036a
Matt827 <122830375+Matt827@users.noreply.github.com>
2023-07-13 18:39:51 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        console.log(`Wrapped ${arr[i]} and added a bow!`)
+        debugger
+    }
+
+    return arr
+}
+
+// wrapGifts(gifts)
+
+function writeCards(arr, eventName) {
+    const newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+        debugger
+    }
+    return newArr
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday")
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--)
+    console.log(i)
+    debugger
+}
+
+countDown(10)
